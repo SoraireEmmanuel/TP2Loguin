f909c9d6077ba7518f3234e902fed36b38d9c660
jonathanr123 <joni_la12_elnacio@hotmail.com>
2020-05-06 02:22:58 -0300

    realize el menu de pestañas de fragments

---
+++ app/src/main/java/com/example/tp2loguin/ui/main/SectionsPagerAdapter.java
@@ -0,0 +1,54 @@
+package com.example.tp2loguin.ui.main;
+
+import android.content.Context;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.example.tp2loguin.Miperfil;
+import com.example.tp2loguin.R;
+import com.example.tp2loguin.Todosperfiles;
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    @StringRes
+    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2};
+    private final Context mContext;
+
+    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        // getItem is called to instantiate the fragment for the given page.
+        // Return a PlaceholderFragment (defined as a static inner class below).
+
+        //RETORNO LAS VISTAS DE CADA FRAGMENTO
+        switch (position){
+            case 0: return new Miperfil();
+            case 1: return new Todosperfiles();
+        }
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mContext.getResources().getString(TAB_TITLES[position]);
+    }
+
+    @Override
+    public int getCount() {
+        // Show 2 total pages.
+        return 2;
+    }
+}
